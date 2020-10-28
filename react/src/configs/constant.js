6034de62eba32a9cc11f07da393786c9ea2dfa72
zaicev-maxim <zaiceffmaksim@gmail.com>
2020-10-29 00:01:45 +0300

    ip configuration

--- react/src/configs/constant.js
+++ react/src/configs/constant.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://localhost:8082"
+export const API_URL = "http://18.224.61.245:8082"
 // export const API_URL = "https://d1d86bbe90bd.ngrok.io"
-export const SOCKET_URL = "localhost:8082"
+export const SOCKET_URL = "18.224.61.245:8082"
 // export const SOCKET_URL = "d1d86bbe90bd.ngrok.io"
 export const RECONNECT_TIMEOUT = 1 * 1000
